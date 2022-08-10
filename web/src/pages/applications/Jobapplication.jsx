3ffbc5ed672358fb133bc4fda3c5cc21e44ad817
Yury Dzhishkariani <dziyura@gmail.com>
2022-08-09 21:48:14 -0700

    jobApplication getuserData setup

--- web/src/pages/applications/Jobapplication.jsx
+++ web/src/pages/applications/Jobapplication.jsx
@@ -10,6 +10,8 @@ import '../employee/css/Employeedetails.css';
 function Jobapplication() {
   const user = useSelector((state) => state.user);
 
+  const [toPostFlag, setToPostFlag] = useState(false);
+
   useEffect(() => {
     getUser(user.user.id).then((response) => {
       if(response){
@@ -23,35 +25,60 @@ function Jobapplication() {
         response.data.contactNo;
         response.data.state;
         response.data.country;
+        response.data.employerNames;
+        response.data.locations;
+        response.data.positions;
+        response.data.start;
+        response.data.end;
+        response.data.descriptions;
       }else {
-        err.response;
+        setToPostFlag(true);
       }
-      console.log(response)
     });
   }, []);
 
-  // const questions = (e) => {
-  //   const emails = [];
-  //   emails.push(email);
-  //   e.preventDefault();
-  //   if(toPostFlag){
-  //     const payload = { 
-  //       id: user.user.id,
-  //       emails,
-  //       name,
-  //       city,
-  //       contactNo: phoneNumber,
-  //     };
-  //     console.log(payload);
-  //     postUser(payload).then((response) => {
-  //       if(!response) {
-  //         toast.error('Cannot add user details');
-  //         return;
-  //       }
-  //     });
-  //   }
-  //   setGotoNextFlag(true);
-  // };
+  const pushApplication = (e) => {
+    const emails = [];
+    emails.push(email);
+    const employerNames = [];
+    employerNames.push(employerName);
+    const locations = [];
+    locations.push(location);
+    const positions = [];
+    positions.push(position);
+    const start = [];
+    start.push(startDate);
+    const end = [];
+    end.push(endDate);
+    const descriptions = [];
+    descriptions.push(description);
+    e.preventDefault();
+    if(toPostFlag){
+      const payload = { 
+        id: user.user.id,
+        name: firstName.concat(' ', lastName),
+        contactNo: mobile,
+        city: city,
+        zip: zipcode,
+        website: website,
+        skills,
+        emails,
+        employerNames,
+        locations,
+        positions,
+        start,
+        end,
+        descriptions,
+      };
+      console.log(payload);
+      postUser(payload).then((response) => {
+        if(!response) {
+          toast.error('Cannot add user details');
+          return;
+        }
+      });
+    }
+  };
   return (
     <div>Success!!!</div>
   );
