2c88fb9bd01bb3f05267b11a71d87ea7586eb560
Yury Dzhishkariani <dziyura@gmail.com>
2022-09-29 19:50:56 -0700

    added table to application. Now need to add CSS

--- web/src/pages/users/components/applicationsTab/ApplicationCard.jsx
+++ web/src/pages/users/components/applicationsTab/ApplicationCard.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-// import InfiniteScroll from 'react-infinite-scroller';
+import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import '../../css/Table.css';
 import getUserApplications from '../../../../api/application/getUserApplications';
 import { makeStyle } from '../../Data/Data';
@@ -27,31 +29,34 @@ const ApplicationCard = () => {
     <div>
       {applications && applications.length > 0
         ? applications.slice(0, 1).map((application) => (
-              <TableRow
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component='th' scope='row'>
-                  {application.job.summary}
-                </TableCell>
-                <TableCell align='left'>
-                  {application.job.company.name}
-                </TableCell>
-                <TableCell align='left'>
-                  {new Date(application.date).toUTCString().slice(4, 16)}
-                </TableCell>
-                <TableCell align='left'>
-                  <span
-                    className='status'
-                    style={makeStyle(application.status)}
-                  >
-                    {application.status}
-                  </span>
-                  {/* <span>{diffDays}</span> */}
-                </TableCell>
-                <TableCell align='left' className='Details'>
-                  Details
-                </TableCell>
-              </TableRow>
+            <TableContainer
+              component={Paper}
+              style={{ boxShadow: '0px 13px 20px 0px #80808029' }}
+            >
+              <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+                <TableRow
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component='th' scope='row'>
+                    {application.job.summary}
+                  </TableCell>
+                  <TableCell align='left'>
+                    {application.job.company.name}
+                  </TableCell>
+                  <TableCell align='left'>
+                    {new Date(application.date).toUTCString().slice(4, 16)}
+                  </TableCell>
+                  <TableCell align='left'>
+                    <span
+                      className='status'
+                      style={makeStyle(application.status)}
+                    >
+                      {application.status}
+                    </span>
+                  </TableCell>
+                </TableRow>
+              </Table>
+            </TableContainer>
           ))
         : null}
     </div>
--- web/src/pages/users/components/mainDash/RightSide.jsx
+++ web/src/pages/users/components/mainDash/RightSide.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 // import CustomerReview from './CustomerReview';
 import Updates from './Updates';
 import '../../css/RightSide.css';
 
 /* eslint-disable */
 const RightSide = () => {
-  const selected = useSelector((state) => state.user.dashboard);
   return (
     <div className='RightSide'>
       <div>
-      {selected === 0  && <h3>Updates</h3>}
-      {selected === 0  && <Updates />}
+      <h3>Updates</h3>
+      <Updates />
       </div>
     </div>
   );
